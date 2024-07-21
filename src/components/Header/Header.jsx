e45cac7c5093d5e0ca86e95f3416893be12109d4
iam-redd <reddou4@mail.ru>
2024-07-21 23:25:06 +0500

    first commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button from "../Button/Button.css";
+import Button from "../Button/Button";
 import {useTelegram} from "../../hooks/useTelegram";
 
 const Header = () => {
