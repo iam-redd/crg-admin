9fb005271720386ff5fb70c36b7e721876c6d80a
iam-redd <reddou4@mail.ru>
2024-07-21 22:45:56 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
   return (
     <div className="App">
       WORKING
-
       <button onClick={onToggleButton}>toggle</button>
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,13 +3,13 @@ import Button from "../Button/Button";
 import {useTelegram} from "../../hooks/useTelegram";
 
 const Header = () => {
-    const {tg, user, onClose} = useTelegram();
+    const {user, onClose} = useTelegram();
 
     return (
         <div className="header">
             <Button onClick={onClose}>Закрыть</Button>
             <span className='username'>
-                {tg.initDataUnsafe?.user?.username}
+                {user?.username}
             </span>
         </div>
     );
