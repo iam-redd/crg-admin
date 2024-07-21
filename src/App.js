1f1224c9293c1435f0a9b1e4c12bfea9c97debec
iam-redd <reddou4@mail.ru>
2024-07-21 22:42:08 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {useEffect} from "react";
-import {useTelegram} from "/hooks/useTelegram";
+import {useTelegram} from "./hooks/useTelegram";
 
 function App() {
     const {tg, onToggleButton} = useTelegram();
